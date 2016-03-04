7f51019518b8a4ca6979440d7bb3fa8d179dea63
swarvanusg <swarvanusg@gmail.com>
2016-03-04 23:44:11 +0530

    Added License header

--- go_jolokia_test.go
+++ go_jolokia_test.go
@@ -1,3 +1,7 @@
+// Copyright © 2014 Swarvanu Sengupta <swarvanusg.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 package go_jolokia
 
 import "testing"
